d4acdcc95c18ec46394b3b105c1ea38a364c3c8a
Yubin <yubinjodev@gmail.com>
2023-08-14 01:56:48 +0900

    adding links

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$(document).ready(function () {
   $("#hamburger").click(() => {
     $(".Menu-root").slideDown();
     $("header").hide();
@@ -6,10 +6,30 @@ $(document).ready(() => {
     $("footer").hide();
   });
 
-  $(".Menu-root button").click(() => {
+  $(".Menu-root button").click(function () {
     $(".Menu-root").slideUp();
     $("header").show();
     $("main").show();
     $("footer").show();
+  });
+
+  $(document).on("click", 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top,
+      },
+      800
+    );
+  });
+
+  $("#contact-btn, #join-btn, #touch-btn").click(function () {
+    $("html").animate(
+      {
+        scrollTop: $("#contact").offset().top,
+      },
+      800
+    );
   });
 });
